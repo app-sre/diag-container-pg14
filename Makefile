5354b45a09decbd85795bc6f5b90c197a51b99d6
RH-tj <155587549+RH-tj@users.noreply.github.com>
2024-02-27 10:32:15 -0500

    bump version (#5)
    Co-authored-by: TJ Carvalho <tcarvalh@tcarvalh-thinkpadp1gen5.remote.csb>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: build login push
 
-IMAGE := quay.io/app-sre/diag-container-pg14:1.0.0
+IMAGE := quay.io/app-sre/diag-container-pg14:110.0
 
 build:
 	@docker build -t $(IMAGE) .
--- pr_check.sh
+++ pr_check.sh
@@ -2,5 +2,5 @@
 set -exv
 docker build  --no-cache \
               --force-rm \
-              -t diag-container-pg14:1.0.0  \
+              -t diag-container-pg14:1.1.0  \
               -f ./Dockerfile .
