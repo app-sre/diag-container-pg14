1a61c7dcc1d01ad836c43436b3577ace67f8f671
RH-tj <155587549+RH-tj@users.noreply.github.com>
2024-03-12 16:32:47 -0400

    bugfix/appsre9816 (#9)
    * bump version
    
    * bugfix/APPSRE9816: fix dnf and bump version
    
    * use latest fedora and bump version
    
    * use fedora 40 image
    
    * use fedora 40
    
    * fix CVE-2024-21626
    
    * bugfix/APPSRE9816: Remove diff from the Dockerfile that are causing FIPS ems issue
    
    ---------
    
    Co-authored-by: TJ Carvalho <tcarvalh@tcarvalh-thinkpadp1gen5.remote.csb>

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 .PHONY: build login push
 
 
-IMAGE := quay.io/app-sre/diag-container-pg14:1.5.0
+
+IMAGE := quay.io/app-sre/diag-container-pg14:1.6.0
 
 build:
 	@docker build -t $(IMAGE) .
--- pr_check.sh
+++ pr_check.sh
@@ -2,5 +2,5 @@
 set -exv
 docker build  --no-cache \
               --force-rm \
-              -t diag-container-pg14:1.5.0  \
+              -t diag-container-pg14:1.6.0  \
               -f ./Dockerfile .
